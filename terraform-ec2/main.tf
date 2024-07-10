51bd7322b346ce5abc2a4ebcaa6f59707c463ab6
Rakib Mustafa <142817958+RakibMustafaRaees@users.noreply.github.com>
2024-07-10 11:58:01 +0000

    Update main.tf

--- terraform-ec2/main.tf
+++ terraform-ec2/main.tf
@@ -1,27 +1,105 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.2"
-    }
+# main.tf
+
+provider "aws" {
+  region = "us-west-2" # Change to your desired region
+}
+
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "my_vpc"
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
+# Create a subnet
 resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a" # Change to your desired availability zone
+
+  tags = {
+    Name = "my_subnet"
+  }
 }
 
-resource "aws_instance" "my_ec2_instance" {
-  ami           = "ami-0cff7528ff583bf9a"
+# Create an internet gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my_igw"
+  }
+}
+
+# Create a route table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "my_route_table"
+  }
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "my_route_table_assoc" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Create a security group
+resource "aws_security_group" "my_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my_sg"
+  }
+}
+
+# Provision an EC2 instance
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0c55b159cbfafe1f0" # Change to your desired AMI
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.my_subnet.id
+  security_groups = [aws_security_group.my_sg.name]
+
+  tags = {
+    Name = "my_instance"
+  }
+}
+
+# Output instance details
+output "instance_id" {
+  value = aws_instance.my_instance.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.my_instance.public_ip
 }
